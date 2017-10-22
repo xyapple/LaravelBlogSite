0cddf22e246473f2cfd939d2f3cca87a10ee54f6
xyapple <ydeascentis@gmail.com>
2017-10-21 23:10:46 -0400

    try to fix the MassAssignmentException

--- app/Post.php
+++ app/Post.php
@@ -6,9 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    //handle the MassAssignmentException error
+    protected $fillable = [
+        'title', 'content', 'category_id','featured'
+    ];
     //a post can only belong to one Category
     public function category()
     {
         return $this->belongsTo(App\Category);
     }
+
 }
