5169573377a28d77a28322b2f4eb350319178376
xyapple <ydeascentis@gmail.com>
2017-10-22 10:23:41 -0400

    use softDeletes() to delete the post but not delete the post from database

--- app/Post.php
+++ app/Post.php
@@ -3,13 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
+
     //handle the MassAssignmentException error
     protected $fillable = [
         'title', 'content', 'category_id','featured'
     ];
+
+    protected $dates=['deleted_at'];
+    
     //a post can only belong to one Category
     public function category()
     {
