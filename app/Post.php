d448ce9c7ec9113600583ee982636d62cfd755c9
xyapple <ydeascentis@gmail.com>
2017-10-22 12:09:10 -0400

    create a tag db and post_tag db

--- app/Post.php
+++ app/Post.php
@@ -24,7 +24,12 @@ class Post extends Model
     //a post can only belong to one Category
     public function category()
     {
-        return $this->belongsTo(App\Category);
+        return $this->belongsTo('App\Category');
+    }
+    //many to many relationship so we have to make a peakV table
+    // tags, posts tables ==> post_tag(we have to create a database table name post_tag)
+    public function tags(){
+        return $this->belongsToMany('App\Tag');
     }
 
 }
