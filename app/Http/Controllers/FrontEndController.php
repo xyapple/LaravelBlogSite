004a18cc899b77e3a20a4e7c3c6eafc490d8d193
xyapple <ydeascentis@gmail.com>
2017-10-26 16:19:30 -0400

    type: feat body: continue working on the single page post

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -29,7 +29,7 @@ class FrontEndController extends Controller
         $post = Post::where('slug', $slug)->first();
         
         return view('single')->with('post', $post)
-                          
+                            ->with('title', $post->title)
                             ->with('settings', Settings::first())
                             ->with('categories', Category::take(5)->get());
     }
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,1 +1,18 @@
 @extends('layouts.frontend')
+
+@section('content')
+
+<div class="container">
+      <div class="stunning-header stunning-header-bg-lightviolet">
+      <div class="stunning-header-content">
+      <h1 class="stunning-header-title">
+      This is title
+      </h1>
+      </div>
+      </div>
+     
+       
+    </div>
+
+
+@endsection
