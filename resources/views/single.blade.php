4df78874acb8103b3bac79095fdc27251b4953e9
xyapple <ydeascentis@gmail.com>
2017-10-26 18:33:09 -0400

    add the prev and next in the controller

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -70,42 +70,69 @@
                               </div>
 
                               <!-- Arthur of the post -->
-                  <div class="blog-details-author">
-                  
-                        <div class="blog-details-author-thumb">
-                              <img src="app/img/blog-details-author.png" alt="Author">
-                        </div>
-                  
-                        <div class="blog-details-author-content">
-                              <div class="author-info">
-                                    <h5 class="author-name">Philip Demarco</h5>
-                                    <p class="author-info">SEO Specialist</p>
+                              <div class="blog-details-author">
+
+                                    <div class="blog-details-author-thumb">
+                                          <img src="{{asset('app/img/blog-details-author.png')}}" alt="Author">
+                                    </div>
+
+                                    <div class="blog-details-author-content">
+                                          <div class="author-info">
+                                                <h5 class="author-name">Philip Demarco</h5>
+                                                <p class="author-info">SEO Specialist</p>
+                                          </div>
+                                          <p class="text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+                                                euismod.
+                                          </p>
+                                          <div class="socials">
+
+                                                <a href="#" class="social__item">
+                                                      <img src="{{asset('app/svg/circle-facebook.svg')}}" alt="facebook">
+                                                </a>
+
+                                                <a href="#" class="social__item">
+                                                      <img src="{{asset('app/svg/twitter.svg')}}" alt="twitter">
+                                                </a>
+
+                                                <a href="#" class="social__item">
+                                                      <img src="{{asset('app/svg/google.svg')}}" alt="google">
+                                                </a>
+
+                                                <a href="#" class="social__item">
+                                                      <img src="{{asset('app/svg/youtube.svg')}}" alt="youtube">
+                                                </a>
+
+                                          </div>
+                                    </div>
+
                               </div>
-                              <p class="text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod.
-                              </p>
-                              <div class="socials">
-                  
-                                    <a href="#" class="social__item">
-                                          <img src="{{asset('app/svg/circle-facebook.svg')}}" alt="facebook">
-                                    </a>
-                  
-                                    <a href="#" class="social__item">
-                                          <img src="{{asset('app/svg/twitter.svg')}}" alt="twitter">
-                                    </a>
-                  
-                                    <a href="#" class="social__item">
-                                          <img src="{{asset('app/svg/google.svg')}}" alt="google">
-                                    </a>
-                  
-                                    <a href="#" class="social__item">
-                                          <img src="{{asset('app/svg/youtube.svg')}}" alt="youtube">
-                                    </a>')}}
-                  
-                              </div>
-                        </div>
-                  </div>
 
                         </article>
+                        <div class="pagination-arrow">
+  
+                              @if($prev)
+                              <a href="{{route('post.single', ['slug'=>$prev->slug])}}" class="btn-next-wrap">
+                                    <div class="btn-content">
+                                          <div class="btn-content-title">Next Post</div>
+                                          <p class="btn-content-subtitle">{{$prev->title}}</p>
+                                    </div>
+                                    <svg class="btn-next">
+                                          <use xlink:href="#arrow-right"></use>
+                                    </svg>
+                              </a>
+                              @endif
+                         @if($next)
+                        <a href="{{route('post.single', ['slug'=>$next->slug])}}" class="btn-prev-wrap">
+                              <svg class="btn-prev">
+                                    <use xlink:href="#arrow-left"></use>
+                              </svg>
+                              <div class="btn-content">
+                                    <div class="btn-content-title">Previous Post</div>
+                                    <p class="btn-content-subtitle">{{$next->title}}</p>
+                              </div>
+                        </a>
+                        @endif
+                        </div>
 
                   </div>
             </main>
