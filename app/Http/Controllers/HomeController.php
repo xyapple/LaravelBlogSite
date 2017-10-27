e1c5de35bf06a9a464babde409c306c6be60eed2
xyapple <ydeascentis@gmail.com>
2017-10-27 10:44:15 -0400

    work on the dashboard for log in

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use App\Category;
+use App\User;
+use App\Tag;
 
 class HomeController extends Controller
 {
@@ -23,6 +27,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.dashboard');
+        return view('admin.dashboard')
+                                    ->with('posts_count', Post::all()->count())
+                                    ->with('trashes_count', Post::onlyTrashed()->get()->count())
+                                    ->with('users_count', User::all()->count())
+                                    ->with('categories_count', Category::all()->count())
+                                    ->with('tags_count', Tag::all()->count());
+                                    
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,15 +1,58 @@
-@extends('layouts.app')
+@extends('layouts.app') @section('content')
 
-@section('content')
-
-           <div class="col-md-3">
-           <div class="panel panel-default">
-           <div class="panel-heading">
-           Dashboard
-           </div>
-           <div class="panel-body">
-           Welcome
-           </div>
-            </div>
-           </div>
+<div class="col-lg-3">
+    <div class="panel panel-info">
+        <div class="panel-heading text-center">
+            Publiced Posts
+        </div>
+        
+        <div class="panel-body">
+            <h1 class="text-center">{{$posts_count}}</h1>
+        </div>
+    </div>
+</div>
+<div class="col-lg-3">
+    <div class="panel panel-danger">
+        <div class="panel-heading text-center">
+            Trushed Posts
+        </div>
+        
+        <div class="panel-body">
+            <h1 class="text-center">{{$trashes_count}}</h1>
+        </div>
+    </div>
+</div>
+<div class="col-lg-3">
+    <div class="panel panel-success">
+        <div class="panel-heading text-center">
+            Users
+        </div>
+        
+        <div class="panel-body">
+            <h1 class="text-center">{{$users_count}}</h1>
+        </div>
+    </div>
+</div>
+<div class="col-lg-3">
+    <div class="panel panel-info">
+        <div class="panel-heading text-center">
+            Categories
+        </div>
+        
+        <div class="panel-body">
+            <h1 class="text-center">{{$categories_count}}</h1>
+        </div>
+    </div>
+</div>
+<div class="col-lg-3">
+    <div class="panel panel-info">
+        <div class="panel-heading text-center">
+            Tags
+        </div>
+        
+        <div class="panel-body">
+            <h1 class="text-center">{{$tags_count}}</h1>
+        </div>
+    </div>
+</div>
 @endsection
