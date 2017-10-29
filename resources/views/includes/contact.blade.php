0b1d5abbafa6cd01c635bb1fee5f0d4f98506d5f
xyapple <ydeascentis@gmail.com>
2017-10-29 16:45:24 -0400

    completed the footer with video background and overly. Video is taken by me from Boston north park

---
+++ resources/views/includes/contact.blade.php
@@ -0,0 +1,28 @@
+ <!-- Contact Section -->
+    <div id="contact" class="container content-section text-center">
+        <div class="row">
+		<div class="col-lg-12">
+		<div class="col-lg-4"></div>
+			<div class="col-lg-4">
+            <h2>Contact Me</h2>
+            <p>Feel free to email me, even just to say hello!</p>
+            <p><a href="mailto:ydeascentis@hotmail.com">ydeascentis@hotmail.com</a>
+            </p>
+            <ul class="list-inline banner-social-buttons">
+                <li>
+                    <a href="www.linkedin.com/in/yindeascentis" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+                </li>
+                <li>
+                    <a href="https://github.com/xyapple" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                </li>
+                <li>
+                    <a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
+                </li>
+            </ul>
+			</div>
+				<div class="col-lg-4"></div>
+			</div>
+        </div>
+
+    </div>
+    <!--contact-->
---
+++ resources/views/includes/video.blade.php
@@ -0,0 +1,29 @@
+<div class="videobody">
+	<div class="container">
+		<div class="row">
+			<div class="background-wrap">
+				<video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted">
+					<source src="{{asset('app/video/xyboston.mp4')}}" type="video/mp4"> Video not supported
+				</video>
+			</div>
+			<div class="col-lg-8 col-lg-offset-2">
+				<div class="content">
+					<div class="heading text-center">
+						<h3>{{ $settings->site_name }}</h3>
+						<div class="heading-line">
+							<span class="short-line"></span>
+							<span class="long-line"></span>
+						</div>
+						<p class="text-center">
+							In here, I try to answer this question: What inspires me? There are a lot of things that can inspire – when we accomplish
+							great things when we overcome adversity when we bring the cameras and document the detail of our journey, even the
+							sheer beauty of nature can remind us just how lucky we are to be alive.
+						</p>
+					
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>
